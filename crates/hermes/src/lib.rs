47ed957d06dc689ffa3bb54d81746843bc2d6592
J Palmer <j-mpalmer@live.com>
2023-12-17 12:18:03 -0400

    Ready. Set. Go

---
+++ crates/hermes/src/lib.rs
@@ -0,0 +1,22 @@
+
+#[derive(Debug)]
+pub enum LogLevel {
+    Error,
+    Warning,
+    Info
+}
+
+impl std::fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{:?}]", self)
+    }
+}
+
+
+pub fn log(level: LogLevel, msg: impl std::fmt::Display) -> () {
+
+    println!("{level}: {msg}")
+
+}
+
+
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+extern crate hermes;
+
+fn main() {
+    hermes::log(hermes::LogLevel::Info, "Hello, World")
+}
