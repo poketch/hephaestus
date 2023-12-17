58782f15a3a7d9183240a320906e40b35db7148f
J Palmer <j-mpalmer@live.com>
2023-12-17 13:15:03 -0400

    Refactor hermes/lib.rs

--- crates/hermes/src/lib.rs
+++ crates/hermes/src/lib.rs
@@ -1,26 +1,18 @@
-pub mod log {
 
-    #[derive(Debug)]
+#[derive(Debug)]
 
-    pub enum LogLevel {
-        Error,
-        Warning,
-        Info
-    }
-
-    impl std::fmt::Display for LogLevel {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "[{:?}]", self)
-        }
-    }
-
-
-    pub fn log(level: LogLevel, msg: impl std::fmt::Display) -> () {
-
-        println!("{level}: {msg}")
-
-    }
-
+pub enum LogLevel {
+    Error,
+    Warning,
+    Info,
 }
 
-pub use log::*; 
+impl std::fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{:?}]", self)
+    }
+}
+
+pub fn log(level: LogLevel, msg: impl std::fmt::Display) -> () {
+    println!("{level}: {msg}")
+}
